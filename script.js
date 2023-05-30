a977434f0cf6bd8f84cfbbf96e23dc447a3bc7cd
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-30 17:56:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ const apiUrls = [
 ];
 
 // You can write your code here
+const outputAll = document.getElementById('output-all');
+const outputAny = document.getElementById('output-any');
+
+function fetchData(apiUrls) {
+	const promises = apiUrls.map(api => fetch(api));
+
+	const allPromise = Promise.all(promises);
+	const anyPromise = Promise.any(promises);
+
+	return {
+		allPromise,
+		anyPromise
+	}
+}
+
+const {allPromise, anyPromise} = fetchData(apiUrls);
+
+const start = new Date().getTime();
+
+allPromise.then(data => {
+	
+	outputAll.innerText = new Date().getTime() - start;
+});
+
+const start1 = new Date().getTime();
+
+anyPromise.then(data => {
+	outputAny.innerText = new Date().getTime() - start1;
+});
+  
